c9284da1e9b3744161f70ae254ea00dbb0d88b06
zz870422zz <zz870422zz@gmail.com>
2017-11-24 13:47:32 +0800

    1124-1

---
+++ htdocs/js/sokoban.js
@@ -0,0 +1,19 @@
+/**
+ *  @file       index.js
+ *  @brief      The entry file of Sokoban.
+ *  @author     zz870422zz (zz870422zz@gmail.com)
+ *  @date       11/17/2017 created.
+ *  @date       11/24/2017 last modified.
+ *  @version    0.1.0
+ *  @copyright  MIT, (C) 2017 zz870422zz
+ *  @details
+ *
+ *  The entry file of Sokoban.
+ */
+'use strict';
+
+window.addEventListener('load', () => {
+  console.log("Sokoban.js loaded");
+});
+
+// index.js
